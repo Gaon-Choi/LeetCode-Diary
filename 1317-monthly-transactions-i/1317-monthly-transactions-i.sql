f6f9d24a7a1866c5c1fc2fd641b56b4f3bc9ccca
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-04-01 23:46:36 +0900

    Time: 587 ms (91.67%), Space: 0B (100%) - LeetHub

--- 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/1317-monthly-transactions-i.sql
@@ -2,7 +2,7 @@
 SELECT  DATE_FORMAT(T1.trans_date, '%Y-%m')                             AS month
     ,   T1.country                                                      AS country
     ,   COUNT(*)                                                        AS trans_count
-    ,   SUM(CASE WHEN T1.state = 'approved' THEN 1 ELSE 0 END)        AS approved_count
+    ,   SUM(CASE WHEN T1.state = 'approved' THEN 1 ELSE 0 END)          AS approved_count
     ,   SUM(T1.amount)                                                  AS trans_total_amount
     ,   SUM(CASE WHEN T1.state = 'approved' THEN T1.amount ELSE 0 END)  AS approved_total_amount
 FROM    Transactions T1
