964fbb9e0c3cc7515a42e79d5e10859b5009b5cd
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-05-01 13:21:37 +0900

    Time: 272 ms (82.63%), Space: 0B (100%) - LeetHub

--- 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
+++ 3530-odd-and-even-transactions/3530-odd-and-even-transactions.sql
@@ -1,12 +1,12 @@
 /* Write your T-SQL query statement below */
 SELECT  transaction_date
     ,   odd_sum =   SUM(
-            CASE    WHEN T1.amount % 2 = 1  THEN T1.amount
+            CASE    WHEN T1.amount & 1 = 1  THEN T1.amount
                     ELSE 0
             END
         )
     ,   even_sum =   SUM(
-            CASE    WHEN T1.amount % 2 = 0  THEN T1.amount
+            CASE    WHEN T1.amount & 1 = 0  THEN T1.amount
                     ELSE 0
             END
         )
