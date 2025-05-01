6fda24aa84eb05b49b9f91ead12e0a3a6e404a29
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-05-01 13:07:24 +0900

    Time: 295 ms (77.43%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -3,19 +3,19 @@ SELECT  TOP 1
         id
     ,   num =   SUM(num)
 FROM    (
-SELECT  id  =   requester_id
-    ,   num =   COUNT(*)
-FROM    RequestAccepted T1
-GROUP BY
-        T1.requester_id
+    SELECT  id  =   requester_id
+        ,   num =   COUNT(*)
+    FROM    RequestAccepted T1
+    GROUP BY
+            T1.requester_id
 
-UNION ALL
+    UNION ALL
 
-SELECT  id  =   accepter_id
-    ,   num =   COUNT(*)
-FROM    RequestAccepted T1
-GROUP BY
-        T1.accepter_id
+    SELECT  id  =   accepter_id
+        ,   num =   COUNT(*)
+    FROM    RequestAccepted T1
+    GROUP BY
+            T1.accepter_id
 ) T
 GROUP BY
         T.id
