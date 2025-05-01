e343dbeca31109d4a0c62bcd598e32aec73f2098
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-05-01 13:06:40 +0900

    Time: 309 ms (67.02%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,23 @@
+/* Write your T-SQL query statement below */
+SELECT  TOP 1
+        id
+    ,   num =   SUM(num)
+FROM    (
+SELECT  id  =   requester_id
+    ,   num =   COUNT(*)
+FROM    RequestAccepted T1
+GROUP BY
+        T1.requester_id
+
+UNION ALL
+
+SELECT  id  =   accepter_id
+    ,   num =   COUNT(*)
+FROM    RequestAccepted T1
+GROUP BY
+        T1.accepter_id
+) T
+GROUP BY
+        T.id
+ORDER BY
+        SUM(num)    DESC
