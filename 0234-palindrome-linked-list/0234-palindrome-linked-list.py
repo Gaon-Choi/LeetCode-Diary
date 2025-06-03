6da1f19ea37a36feca1ac929420ab3e4de77c3bf
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-06-03 23:22:27 +0900

    Time: 27 ms (73.66%), Space: 39.9 MB (9.05%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,12 +9,14 @@ class Solution:
 
         node = head
 
+        # 링크드리스트를 순회하며 차례대로 스택에 넣음
         while node:
             stk.append(node.val)
             node = node.next
 
         node = head
 
+        # 링크드리스트를 다시 순회하며 스택의 top과 비교
         while node:
             if node.val != stk.pop():
                 return False
