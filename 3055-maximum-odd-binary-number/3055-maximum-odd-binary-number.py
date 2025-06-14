9e6019a9ce887d2166b9918b2a8c3fb50625e1e2
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-06-15 04:51:44 +0900

    Time: 0 ms (100%), Space: 18 MB (15.87%) - LeetHub

---
+++ 3055-maximum-odd-binary-number/3055-maximum-odd-binary-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        zeros = s.count("0")
+
+        ones = len(s) - zeros - 1
+
+        return ones * "1" + zeros * "0" + "1"
+        
