7d795dea612cc703a438a338598c5a5cebb25f9f
최가온(Gaon Choi) <46679380+Gaon-Choi@users.noreply.github.com>
2025-06-18 07:05:40 +0900

    Time: 0 ms (100%), Space: 17.6 MB (97.47%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        idx = -1
+
+        for i in range(len(word)):
+            if word[i] == ch:
+                idx = i
+                break
+
+        if idx == -1:
+            return word
+
+        return word[:i + 1][-1::-1] + word[i + 1:]
